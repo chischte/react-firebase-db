38bb3b7f1a1afdf1ce2470c73c386f7f913c344e
Michael Wettstein <chischte@gmx.ch>
2020-08-09 17:36:31 +0200

    Add config.js

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+  apiKey: "AIzaSyCbaayy1kDUNcRmSsuhUb6wz6pAY9bSJbY",
+  authDomain: "react-fir-login-tutorial.firebaseapp.com",
+  databaseURL: "https://react-fir-login-tutorial.firebaseio.com"
+};
+
+export default config;
